4c38d8bdbb9350701acd26aba316953d57b9865f
Choi Hanna <gkssk4163@hanmail.net>
2022-10-12 16:07:38 +0900

    Spring Web 프로젝트 생성

---
+++ BackEnd/src/main/java/com/dokb/DoKB/DoKbApplication.java
@@ -0,0 +1,13 @@
+package com.dokb.DoKB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoKbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoKbApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/dokb/DoKB/DoKbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokb.DoKB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoKbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
