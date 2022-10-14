42c2806b3be775a70b5eea9451a4e2078fd3bee5
Choi Hanna <gkssk4163@hanmail.net>
2022-10-14 14:40:01 +0900

    feat: swagger-ui route 설정 추가 - /api/usage 로 접근 가능하도록 변경

--- BackEnd/src/main/java/com/dokb/DoKB/DoKbApplication.java
+++ BackEnd/src/main/java/com/dokb/DoKB/DoKbApplication.java
@@ -2,12 +2,26 @@ package com.dokb.DoKB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
 public class DoKbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoKbApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routerFunction() {
+		return route(GET("/api/usage"), req ->
+				ServerResponse.temporaryRedirect(URI.create("/swagger-ui.html")).build());
 	}
 
 }
