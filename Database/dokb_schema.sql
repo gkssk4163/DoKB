59f3fad10a47e2bdbb2b1cdbeaafeafdf85aec80
waterricecake <fruturum@nate.com>
2022-10-14 17:18:41 +0900

    fix: in_out

--- Database/dokb_schema.sql
+++ Database/dokb_schema.sql
@@ -1,120 +1,88 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dokb
+-- ------------------------------------------------------
+-- Server version	8.0.30
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema pointer
--- -----------------------------------------------------
+--
+-- Table structure for table `account`
+--
 
--- -----------------------------------------------------
--- Schema pointer
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `pointer` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
--- -----------------------------------------------------
--- Schema dokb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema dokb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `dokb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `pointer` ;
-
--- -----------------------------------------------------
--- Table `pointer`.`user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pointer`.`user` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `account` VARCHAR(50) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
-  `password` VARCHAR(50) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `created_by` VARCHAR(20) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
-  `updated_at` DATETIME NULL DEFAULT NULL,
-  `updated_by` VARCHAR(20) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 26
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_bin;
-
-
--- -----------------------------------------------------
--- Table `pointer`.`user_data`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pointer`.`user_data` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `eye_pos` FLOAT NOT NULL,
-  `eye_blink` FLOAT NOT NULL,
-  `nod_rl` INT NOT NULL,
-  `nod_fr` INT NOT NULL,
-  `stt` TEXT CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `created_by` VARCHAR(20) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
-  `user_id` BIGINT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_bin;
-
-USE `dokb` ;
-
--- -----------------------------------------------------
--- Table `dokb`.`user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dokb`.`user` (
-  `register_number` INT NOT NULL,
-  `name` VARCHAR(10) NOT NULL,
-  `phone_number` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `address` VARCHAR(100) NOT NULL,
-  `job` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`register_number`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `dokb`.`account`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dokb`.`account` (
-  `password` VARCHAR(100) NOT NULL,
-  `account_number` VARCHAR(100) NOT NULL,
-  `purpose` VARCHAR(45) NOT NULL,
-  `sof` VARCHAR(45) NOT NULL DEFAULT 'source of funds',
-  `balance` INT NOT NULL,
-  `user_register_number` INT NOT NULL,
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account` (
+  `account_number` varchar(30) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `purpose` varchar(45) NOT NULL,
+  `sof` varchar(45) NOT NULL COMMENT '''source of funds''',
+  `balance` bigint NOT NULL DEFAULT '0',
+  `user_register_number` varchar(14) NOT NULL,
   PRIMARY KEY (`account_number`),
-  INDEX `fk_account_user_idx1` (`user_register_number` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  KEY `fk_account_user_idx` (`user_register_number`),
+  CONSTRAINT `fk_account_user` FOREIGN KEY (`user_register_number`) REFERENCES `user` (`register_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `history`
+--
 
--- -----------------------------------------------------
--- Table `dokb`.`history`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dokb`.`history` (
-  `id` INT NOT NULL,
-  `date` DATETIME NOT NULL,
-  `opponent` VARCHAR(100) NOT NULL,
-  `amount` VARCHAR(45) NOT NULL,
-  `inout` CHAR(1) NOT NULL,
-  `balance` VARCHAR(45) NULL DEFAULT NULL,
-  `account_account_number1` VARCHAR(100) NOT NULL,
+DROP TABLE IF EXISTS `history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `history` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `opponent_account` varchar(30) NOT NULL,
+  `deal_date` datetime NOT NULL,
+  `in_out` char(1) NOT NULL COMMENT 'i: 입금 o: 출금',
+  `amount` bigint NOT NULL,
+  `balance` bigint NOT NULL,
+  `account_number` varchar(30) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_history_account1_idx1` (`account_account_number1` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  KEY `fk_history_account1_idx` (`account_number`),
+  CONSTRAINT `fk_history_account1` FOREIGN KEY (`account_number`) REFERENCES `account` (`account_number`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `user`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `register_number` varchar(14) NOT NULL,
+  `name` varchar(10) NOT NULL,
+  `phone_number` varchar(13) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `job` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`register_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-14 17:16:50
